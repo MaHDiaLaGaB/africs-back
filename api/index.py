ba5f31a1bc328ba748f14288c9be53e9803355c4
MaHDiaLaGaB <quads.expanse.0j@icloud.com>
2025-07-11 19:45:38 +0200

    deploy-2

--- api/index.py
+++ api/index.py
@@ -1,8 +1,1 @@
-# # Add project root to Python path
-# import sys
-# from pathlib import Path
-# sys.path.append(str(Path(__file__).parent.parent))
-# from mangum import Mangum
-# from app.main import app_
-
-# handler = Mangum(app_)
+from app.main import app  # noqa: F401
